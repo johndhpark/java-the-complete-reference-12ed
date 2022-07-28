3a72ced80c75e81a008480af39fed6d44fdb594c
John DH. Park <john.dh.park@me.com>
2022-07-28 08:48:34 -0700

    Chapter 7: A Closer Look at Methods and Classes

---
+++ Ch_07/StringDemo3/StringDemo3.java
@@ -0,0 +1,11 @@
+// Demonstrate String arrays
+
+class StringDemo3 {
+	public static void main(String[] args) {
+		String[] strs = { "one", "two", "three" };
+
+		for (int i = 0; i < strs.length; i++) {
+			System.out.println(strs[i]);
+		}
+	}
+}
---
+++ Ch_07/VarArgs2/VarArgs2.java
@@ -0,0 +1,20 @@
+public class VarArgs2 {
+
+	// Here msg is a normal parameter and v is a
+	// varargs parameter
+	static void vaTest(String msg, int... v) {
+		System.out.print(msg + v.length + " Contents: ");
+
+		for (int x : v) {
+			System.out.print(x + " ");
+		}
+
+		System.out.println();
+	}
+
+	public static void main(String[] args) {
+		vaTest("One varargs", 10);
+		vaTest("Three varargs", 1, 2, 3);
+		vaTest("No varargs");
+	}
+}
