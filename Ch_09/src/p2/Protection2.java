c0709bd93ea302248f3c92ed5703da319bd2a22f
John DH. Park <john.dh.park@me.com>
2022-08-02 22:26:01 -0700

    Cleanup

--- Ch_09/src/p2/Protection2.java
+++ Ch_09/src/p2/Protection2.java
@@ -6,11 +6,11 @@ public class Protection2 extends p1.Protection {
 
         System.out.println("n_pro = " + n_pro);
 
-//      class or package only
-//      System.out.println("n = " + n);
+        // class or package only
+        // System.out.println("n = " + n);
 
-//      class only
-//      System.out.println("n_pri = " + n_pri);
+        // class only
+        // System.out.println("n_pri = " + n_pri);
 
         System.out.println("n_pro = " + n_pro);
         System.out.println("n_pub = " + n_pub);
