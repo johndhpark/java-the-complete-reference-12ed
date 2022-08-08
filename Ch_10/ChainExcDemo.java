7fdc6b2bb08fed1ed8f5ed68b99600b44b0937dd
John DH. Park <john.dh.park@me.com>
2022-08-08 15:38:56 -0700

    Chapter 10: pg. 239 to 246

---
+++ Ch_10/ChainExcDemo.java
@@ -0,0 +1,23 @@
+class ChainExcDemo {
+	static void demoproc() {
+		// create an exception
+		NullPointerException e = new NullPointerException("top layer");
+
+		// add a cause
+		e.initCause(new ArithmeticException("cause"));
+
+		throw e;
+	}
+
+	public static void main(String[] args) {
+		try {
+			demoproc();
+		} catch (NullPointerException e) {
+			// dispaly top level exception
+			System.out.println("Caught: " + e);
+
+			// display cause exception
+			System.out.println("Original cause: " + e.getCause());
+		}
+	}
+}
