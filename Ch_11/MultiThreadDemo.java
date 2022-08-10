acb0c694807e6805c3dc2f5161c560e5aa8fe8d9
John DH. Park <john.dh.park@me.com>
2022-08-10 06:23:19 -0700

    Chapter 10: pg. 247 to 260

---
+++ Ch_11/MultiThreadDemo.java
@@ -0,0 +1,45 @@
+// Create a multiple threads.
+class NewThread implements Runnable {
+	String name; // name of thread
+	Thread t;
+
+	NewThread(String threadName) {
+		name = threadName;
+		t = new Thread(this, name);
+		System.out.println("New thread: " + t);
+	}
+
+	// This is the entry point for thread
+	public void run() {
+		try {
+			for (int i = 5; i > 0; --i) {
+				System.out.println(name + ": " + i);
+				Thread.sleep(1000);
+			}
+		} catch (InterruptedException e) {
+			System.out.println(name + "Interrupted");
+		}
+		System.out.println(name + " exiting");
+	}
+}
+
+class MultiThreadDemo {
+	public static void main(String[] args) {
+		NewThread nt1 = new NewThread("One");
+		NewThread nt2 = new NewThread("Two");
+		NewThread nt3 = new NewThread("Three");
+
+		nt1.t.start();
+		nt2.t.start();
+		nt3.t.start();
+
+		try {
+			// wait for other threads to end
+			Thread.sleep(10000);
+		} catch (InterruptedException e) {
+			System.out.println("Main thread Interrupted");
+		}
+
+		System.out.println("Main thread exiting");
+	}
+}
