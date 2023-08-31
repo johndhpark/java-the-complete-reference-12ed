25732500f5a85f47c11ce0988e48abe1344c3f84
John DH. Park <john.dh.park@icloud.com>
2023-08-31 13:48:52 -0700

    Chapter 22: pg. 713 to 720

---
+++ Ch_22/DirListOnly.java
@@ -0,0 +1,29 @@
+// Directory of .HTML files
+
+import java.io.*;
+
+public class DirListOnly {
+    public static void main(String[] args) {
+        String dirname = "/Users/johnpark/practice_java";
+        File f1 = new File(dirname);
+        FilenameFilter only = new OnlyExt("html");
+
+        String[] s = f1.list(only);
+
+        for (int i = 0; i < s.length; i++) {
+            System.out.println(s[i]);
+        }
+    }
+}
+
+class OnlyExt implements FilenameFilter {
+    String ext;
+
+    public OnlyExt(String ext) {
+        this.ext = "." + ext;
+    }
+
+    public boolean accept(File dir, String name) {
+        return name.endsWith(ext);
+    }
+}
